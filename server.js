84a7c8ada30c500b8925fad02a9ea73939324b4d
Emiliano <a01710711@tec.mx>
2025-04-18 19:50:12 -0600

    Agrega la estructura basica del endpoint para registrar charola, tambien se agrega un modelo generico

--- server.js
+++ server.js
@@ -1,40 +1,52 @@
-const express = require('express');
-const path = require('path');
+/* Dependencias */
+const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const session = require("express-session");
-const app = express();
+const db = require("./utils/database");
+/* ------------ */
 
-const db = require('./utils/database');
+/* Rutas */
+const charolaRoutes = require("./routes/charola.routes");
+/* ----- */
 
-async function testDB(){
-    try {
-        const test = await db();
-        console.log('Conexión exitosa. Resultado:', test);
-    } catch (error) {
-        console.error('Error al conectar con la base de datos:', error);
-    }
+/* Rutas de la API */
+app.use("/charola", charolaRoutes);
+/* ----- */
+
+/* Conexion a la base de datos*/
+async function testDB() {
+  try {
+    const test = await db();
+    console.log("Conexión exitosa. Resultado:", test);
+  } catch (error) {
+    console.error("Error al conectar con la base de datos:", error);
+  }
 }
 
 testDB();
+/* ----- */
 
-app.set('view engine', 'ejs');
+/* Inicializar express */
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+/* ----- */
 
-// Route 404 Error
+/* Route 404 Error */
 app.use((req, res, next) => {
-    res.status(404).render("404", { title: "404 - Not Found" });
+  res.status(404).render("404", { title: "404 - Not Found" });
 });
 app.use((req, res) => {
-    console.log("invalid url");
-    res.redirect("/");
+  console.log("invalid url");
+  res.redirect("/");
 });
-  
-/* Start server */
+/* ----- */
+
+/* Iniciar server */
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-console.log(`Servidor corriendo en http://localhost:${port}`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
-  
-
+/* -------- */

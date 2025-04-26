5f696dbd3b4158f9f1f57d80ae23422ec4e7c62d
Sofia Osorio <a01277695@tec.mx>
2025-04-25 18:50:10 -0600

    Referencia en el código - PR11

--- controllers/menu_charolas.controller.js
+++ controllers/menu_charolas.controller.js
@@ -1,3 +1,5 @@
+// RF16 Visualizar todas las charolas registradas en el sistema - https://codeandco-wiki.netlify.app/docs/proyectos/larvas/documentacion/requisitos/RF16
+
 const Charola = require('../models/menu_charolas.model');
 
 const obtenerCharolas = async (req, res) => {
--- routes/menu_charolas.route.js
+++ routes/menu_charolas.route.js
@@ -1,3 +1,5 @@
+// RF16 Visualizar todas las charolas registradas en el sistema - https://codeandco-wiki.netlify.app/docs/proyectos/larvas/documentacion/requisitos/RF16
+
 const express = require('express');
 const router = express.Router();
 const { obtenerCharolas } = require('../controllers/charolas.controlador');

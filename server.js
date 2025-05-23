109e50deaf8e16ccceb4381277d49dac0c048d11
AntonioLanderos <a00574000@tec.mx>
2025-05-22 23:37:58 -0600

    Añadir middleware para limitar requests

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const sesionActiva = require('./utils/middlewares/sesionActiva');
+const limitarAPI = require('./utils/middlewares/limitarAPI');
 /* ------------ */
 
 /* Rutas */

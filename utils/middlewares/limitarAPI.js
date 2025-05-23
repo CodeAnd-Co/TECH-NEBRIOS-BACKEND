db9eba9668ab143a550ddfc16bbda2e3a9a392c3
AntonioLanderos <a00574000@tec.mx>
2025-05-22 23:50:24 -0600

    Cambiar límite a 200 para pruebas

--- utils/middlewares/limitarAPI.js
+++ utils/middlewares/limitarAPI.js
@@ -2,13 +2,13 @@ const rateLimit = require('express-rate-limit');
 /**
  * Middleware para limitar la cantidad de peticiones a la API.
  * 
- * Limita a 500 peticiones por IP cada 15 minutos.
+ * Limita a 200 peticiones por IP cada 15 minutos.
  * 
  * @module apiLimiter
  */
 const limitarAPI = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 100,   
+  max: 200,   
   keyGenerator: (req) => {
     return req.ip;
   },

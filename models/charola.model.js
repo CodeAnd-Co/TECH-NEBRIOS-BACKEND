f0ced9680d3aa37c1c342aca5a9dbdb4266af85b
slugcraft <164267006+slugcraft@users.noreply.github.com>
2025-05-27 01:24:09 -0600

    Se modifico el editar el estado y densidad de una charola

--- models/charola.model.js
+++ models/charola.model.js
@@ -138,7 +138,7 @@ module.exports = class Charola {
         data: {
           nombreCharola: charola.get('nombreCharola'),
           fechaCreacion: charola.get('fechaCreacion'),
-          pesoCharola: charola.get('pesoCharola'),
+          densidadLarva: charola.get('densidadLarva'),
           estado: charola.get('estado'),
           fechaActualizacion: charola.get('fechaActualizacion'),
         },
